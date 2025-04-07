3410e03e9b6716e4bc6c9249841abb0fa14f6443
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2025-04-07 23:17:55 +0300

    changed network manager for async await and added navbar

---
+++ ShopApp-SwiftUI/Feature/Basket/BasketView.swift
@@ -0,0 +1,18 @@
+//
+//  BasketView.swift
+//  ShopApp-SwiftUI
+//
+//  Created by Osmancan Akagündüz on 7.04.2025.
+//
+
+import SwiftUI
+
+struct BasketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasketView()
+}
